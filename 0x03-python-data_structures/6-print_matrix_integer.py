b7efaff1b132801d5f25f33cb7c0763584547698
Mebruk19 <101485086+Mebruk19@users.noreply.github.com>
2022-07-06 01:27:22 +0300

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
-# 6-print_matrix_integer.py
-# mebruk abdu
-
-
 def print_matrix_integer(matrix=[[]]):
-    """Print a matrix of integers."""
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j != (len(matrix[i]) - 1):
-                    print(" ", end="")
-
-        print("")
+    for i in matrix:
+        print(' '.join('{:d}'.format(j)for j in i))
+        
